d5966f63a30454ca5c2a081cc59d037a2ffa0e6c
Blaine Bublitz <blaine.bublitz@gmail.com>
2016-08-15 19:25:34 -0700

    Breaking: Drop node <0.10 support, switch to mocha + expect for testing

--- test/main.js
+++ test/main.js
@@ -1,51 +1,50 @@
-var replaceExt = require('../');
+'use strict';
+
 var path = require('path');
-var should = require('should');
-require('mocha');
+
+var expect = require('expect');
+
+var replaceExt = require('../');
 
 describe('replace-ext', function() {
-  it('should return a valid replaced extension on nested', function(done) {
+
+  it('returns a valid replaced extension on long path', function(done) {
     var fname = path.join(__dirname, './fixtures/test.coffee');
     var expected = path.join(__dirname, './fixtures/test.js');
-    var nu = replaceExt(fname, '.js');
-    should.exist(nu);
-    nu.should.equal(expected);
+    var result = replaceExt(fname, '.js');
+    expect(result).toEqual(expected);
     done();
   });
 
-  it('should return a valid replaced extension on flat', function(done) {
+  it('returns a valid replaced extension on basename', function(done) {
     var fname = 'test.coffee';
     var expected = 'test.js';
-    var nu = replaceExt(fname, '.js');
-    should.exist(nu);
-    nu.should.equal(expected);
+    var result = replaceExt(fname, '.js');
+    expect(result).toEqual(expected);
     done();
   });
 
   it('should not return a valid replaced extension on empty string', function(done) {
     var fname = '';
     var expected = '';
-    var nu = replaceExt(fname, '.js');
-    should.exist(nu);
-    nu.should.equal(expected);
+    var result = replaceExt(fname, '.js');
+    expect(result).toEqual(expected);
     done();
   });
 
-  it('should return a valid removed extension on nested', function(done) {
+  it('returns a valid removed extension on long path', function(done) {
     var fname = path.join(__dirname, './fixtures/test.coffee');
     var expected = path.join(__dirname, './fixtures/test');
-    var nu = replaceExt(fname, '');
-    should.exist(nu);
-    nu.should.equal(expected);
+    var result = replaceExt(fname, '');
+    expect(result).toEqual(expected);
     done();
   });
 
-  it('should return a valid added extension on nested', function(done) {
+  it('returns a valid added extension on long path', function(done) {
     var fname = path.join(__dirname, './fixtures/test');
     var expected = path.join(__dirname, './fixtures/test.js');
-    var nu = replaceExt(fname, '.js');
-    should.exist(nu);
-    nu.should.equal(expected);
+    var result = replaceExt(fname, '.js');
+    expect(result).toEqual(expected);
     done();
   });
 });
